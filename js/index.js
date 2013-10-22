4e631d4aa754dd314f0dc89af53cb373a04b4cc1
CK-Kyle <kyle.kosik@chatham-kent.ca>
2013-10-22 08:57:53 -0400

    Added Google Analytics tracking code

--- js/index.js
+++ js/index.js
@@ -273,3 +273,12 @@
 	function onError(compassError) {
 		alert('Compass error: ' + compassError.code);
 	}
+	
+	// Google Analytics Tracking Code	
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-45043814-1', 'chatham-kent.ca');
+	ga('send', 'pageview');
