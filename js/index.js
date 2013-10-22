fc20514d31f139a5861f8c6265d0aa75dd65ad7a
CK-Kyle <kyle.kosik@chatham-kent.ca>
2013-10-22 13:12:48 -0400

    Removed files/GA

--- js/index.js
+++ js/index.js
@@ -72,12 +72,12 @@
 		console.log("loading gmaps...")
 		//google.maps.event.addDomListener(window, 'load', initialize);
 	});
-	
+	/*
 	$(document).on("pageload", '#page-compass-test', function(){
 		console.log("loading compass...")
 		startWatch();
 	});
-	
+	*/
 	$(document).on('pageload', '#Page-Map', function(event){
 		console.log("loading gmaps...");
 		google.maps.event.addDomListener(window, 'load', initialize);
@@ -275,6 +275,7 @@
 	}
 	
 	// Google Analytics Tracking Code	
+	/*
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -282,3 +283,5 @@
 
 	ga('create', 'UA-45043814-1', 'chatham-kent.ca');
 	ga('send', 'pageview');
+	
+	*/
